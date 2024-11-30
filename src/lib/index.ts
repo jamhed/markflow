e6745985629d50b82fcacbce7bbcd2b56d90bef7
Roman Galeev <jamhedd@gmail.com>
2024-11-30 12:39:29 +0100

    fix: Add exports, rearrange deps

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
 // Reexport your entry components here
+export * from './code.js';
+export * from './files.js';
+export * from './git.js';
+export * from './links.js';
+export * from './meta.js';
+export * from './parser.js';
+export * from './processor.js';
+export * from './rss.js';
+export * from './sitemap.js';
